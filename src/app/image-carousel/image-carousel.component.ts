914e6838077f70576e7adf06a583e87afbe10365
offline2099 <offline2099@protonmail.com>
2023-11-14 18:47:10 +0700

    Minor refactoring

--- src/app/image-carousel/image-carousel.component.ts
+++ src/app/image-carousel/image-carousel.component.ts
@@ -1,29 +1,24 @@
-import { Component, OnInit, Input, HostBinding, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Component, Input, HostBinding, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.css']
 })
-export class ImageCarouselComponent implements OnInit, OnChanges {
+export class ImageCarouselComponent {
 
   constructor() { }
 
-  @Input() type: string = 'normal';
   @Input() folderPath: string = '';
   @Input() imageNames: string[] = [];
   @Input() selectedImage: number = 1;
 
-  @HostBinding('class.inside-overlay') isOverlay: boolean = false;
+  @HostBinding('class.inside-overlay') @Input() isOverlay: boolean = false;
 
   @Output() imageChanged = new EventEmitter<number>();
 
   currentImage: number = 1;
   overlayActive: boolean = false;
-
-  ngOnInit(): void {
-    if (this.type == 'overlay') this.isOverlay = true;
-  }
 
   ngOnChanges(): void {
     this.currentImage = this.selectedImage;
