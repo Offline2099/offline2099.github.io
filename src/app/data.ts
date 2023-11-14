413bafad3236f5a70fd44b3d2df5a0b6ab561bdc
offline2099 <offline2099@protonmail.com>
2023-11-14 18:47:34 +0700

    Update project descriptions

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,11 +1,11 @@
 import { WebsiteData } from './interfaces';
 
 export const Intro: string[] = [
-  'If you are looking at this page, you have probably seen one of my websites and got curious to see more. Or maybe you are already looking at my resume and considering to hire me for your project.',
-  'I know how to build neat and well-functioning websites, and I enjoy doing that. I can talk about my work for hours, but I\'m not really good at convincing people, nor do I enjoy the process of persuading someone. The easiest way for me is to show some examples, so anyone could just see and decide whether it fits their expectations.',
-  'For a web developer, especially a freelancer, it is not a trivial task to build a reliable portfolio. People pay for their new website, then their business fails for some reason, and they end up shutting down the website after a year. The work disappears without a trace.',
+  'If you are looking at this page, you have probably seen one of my websites and found yourself curious to see more. Or maybe you are already looking at my resume and considering to hire me for your project.',
+  'I know how to build neat and well-functioning websites. I enjoy what I do, and I can talk about my work for hours. But I\'m not really good at convincing people, nor do I like the process of persuading someone. The easiest way for me is to show some examples, so anyone interested could just check it out and decide whether it fits their expectations and quality standards.',
+  'For a web developer, especially a freelancer, it is not a trivial task to build a reliable portfolio. People pay for their new website, then their business fails for some reason, and they shut it down after a year. All the work disappears, sometimes without a trace. In addition, web interfaces are often designed only for internal use within a company.',
   'To combat this annoying issue, I rely on using my personal projects as a proof of my skills. This way I can also be sure that no unexpected changes will appear on those websites, no legal restrictions or NDAs will apply, and no questionable ideas implemented by others will be attributed to me.',
-  'I have also created a number of mini-projects, mostly for the demonstration purposes, and I will probably add more of such projects here later. Enjoy having a look at the list below.'
+  'I have also created a number of mini-projects, mostly for the demonstration purposes, and I will probably keep adding more of such projects here later. For each project there is a GitHub repository link available. Enjoy having a look at the list below.'
 ];
 
 export const Websites: WebsiteData[] = [
@@ -18,13 +18,13 @@ export const Websites: WebsiteData[] = [
       n: 6
     },
     description: {
-      summary: 'An image gallery with hundreds of photos taken by me while living in Thailand over the last several years. The same interface can be possibly used to organize and display a collection of galleries of any other images.',
+      summary: 'An image gallery with hundreds of photos taken by me while living in Thailand over the last several years. The same interface can possibly be used for organizing and displaying a collection of any other photo albums or image sets.',
       features: [
-        'The main idea of this project is designing a comfortable interface for viewing a large amount of images with some data attached to them. The data is used both for organizing the photos and for providing additional context.',
-        'All photos are sorted into galleries by the years and months they were taken and by the locations where they were taken. Each image also has several tags, which may include the weather, time of day, or some features present in the photo. Pictures can be grouped into galleries by these tags.',
-        'Each image has a caption. Those are mostly boring, but explain what exactly the user is looking at. This information may be valuable in some cases.',
-        'There are several ways to view the same gallery, and the user can easily switch between those. Each image can also be easily viewed in full size or downloaded.',
-        'There are some experimental features, such as the no-click mode that allows to switch between images just by hovering over the preview.',
+        'The main idea of this project is designing a convenient interface for viewing a large amount of images with some data attached to them. The data is used both for organizing the photos and for providing additional context.',
+        'All photos are sorted into galleries by the time and locations at which they were taken. Each image also has several tags, which may include the weather, time of day, or some features present in the photo. Pictures can be automatically grouped into galleries by each of these tags.',
+        'Each image has a caption. Those are mostly boring, but explain what exactly is shown in the photo. This information may be interesting in some cases.',
+        'The user can always switch between several different ways of viewing the same gallery. Each image can also be easily viewed in full size or downloaded.',
+        'There are some experimental features, such as the no-click mode that allows to switch between images by simply hovering over the preview.',
         'The interface is not cluttered with unnecessary junk, such as likes or social buttons. Most of the existing features (such as captions and tags) can also be completely hidden.'
        ]
     }
@@ -37,13 +37,13 @@ export const Websites: WebsiteData[] = [
       n: 9
     },
     description: {
-      summary: 'A website that contains strategies for a popular multiplayer video game, along with various other related information.',
+      summary: 'A website that explains strategies for a popular multiplayer video game, along with various other related information.',
       features: [
         'Arguably, the best available website on this particular topic. The content may be of no interest to the general public, although it receives mostly positive reactions from people familiar with the game.',
         'The site also serves as a polygon for experimenting with design, visual effects, various ways of organizing and presenting complex information, implementing some algorithms, new features of the framework, etc.',
         'The total volume of data is quite large. The site has hundreds of pages and utilizes dynamic routing. Nested collapsible blocks are used extensively to display the information in a well-structured and easily accessible way.',
-        'Raid strategies are presented in the form of nested lists. This also demonstrates how nested lists can be used to convey a description of a complex process in a more convenient form than traditional text written as paragraphs could achieve.',
-        'Another notable part of the site is the lists of rewards for each fight, called "loot tables". These tables contain a large number of items with multiple parameters. The items can be sorted, grouped, or filtered in a variety of ways, depending on the user\'s needs or preferences.'
+        'Raid strategies are presented in the form of nested lists. This also demonstrates how nested lists can be used to convey a description of a complex process and show its structure in a more convenient and intuitive way than traditional text written as paragraphs could achieve.',
+        'Another notable part of the site is the lists of rewards for each fight, called "loot tables". These tables contain a large number of items with multiple parameters. The items can be sorted, grouped, or filtered in a variety of ways, depending on the user\'s needs and preferences.'
       ]
     }
   },
@@ -58,10 +58,11 @@ export const Websites: WebsiteData[] = [
     description: {
       summary: 'A brief summary of the Earth\'s geologic history. Shows the structure of the geologic time scale in a simple interactive way. Gives a basic understanding of the key features and events that characterize each time interval.',
       features: [
-        'The core idea is to display the Earth\'s geologic history in a way that is both informative and visually enjoyable, while maintaining a reasonable balance between providing a meaningful overview and going into too much detail.',
+        'The core idea is to display the Earth\'s geologic history in a way that is both informative and visually enjoyable, providing a meaningful overview without going into too much detail.',
         'Nested collapsible blocks offer a compact and intuitive layout for viewing and navigating the timeline structure, also making it easier to remember.',
-        'The timeline can be viewed in either direction: chronologically (starting from the formation of the planet) or inversely (diving from the present into older and older epochs).',
-        'There is a quick navigation panel on the right that allows the user to jump to any specific timeline division with a single click.'
+        'The timeline can be viewed in either direction: chronologically (starting from the formation of the planet) or inversely (diving from the present into older periods and epochs).',
+        'There is a quick navigation panel on the right that allows the user to jump to any specific timeline division with a single click.',
+        'The site usilizes Angular components recursively. This approach allows to display a fairly large structure with relatively little code.'
       ]
     }
   },
@@ -111,7 +112,7 @@ export const Websites: WebsiteData[] = [
     description: {
       summary: 'An easy-to-use color converter. Works with RGB, HSL, and CMYK color spaces. Includes multiple formats for each color space. Can also be used as a color picker.',
       features: [
-        'Allows multiple ways to input the color: with sliders for any of the three color spaces, or as text in multiple notations (including percentage and hexadecimal).',
+        'Allows multiple ways to input the color: with sliders for any of the three color spaces, or as text in multiple notations (including percentages and hexadecimal).',
         'Automatically outputs the converted color in all three color spaces (RGB, HSL, CMYK) in all the most commonly used notations.',
         'Provides an interactive visual demonstration of how changing a color in one format affects the representation of that color in other formats.',
         'A complex step-by-step validation for text input. Shows exactly what is wrong in case of an error. Provides warnings for valid but odd input.',
