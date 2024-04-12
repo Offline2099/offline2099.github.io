77bdbba721cf2e9d86c5ea0c9f06ab628c872983
offline2099 <offline2099@protonmail.com>
2024-04-12 18:16:38 +0700

    Update project descriptions

--- src/app/data.ts
+++ src/app/data.ts
@@ -86,9 +86,11 @@ export const Websites: WebsiteData[] = [
       summary: 'A simple yet powerful application that displays the calendar for any year between 20,000\xa0BC (ice age) and 20,000\xa0AD (deep future) and some additional info.',
       features: [
         'Has a simple and intuitive UI for selecting any year within multiple millennia. The interface adapts smoothly to small screens of mobile devices.',
-        'Has a menu to choose which weekday should be treated as the first day of the week (and works for any possible weekday).',
-        'For any day in the calendar grid it shows how many years and how many days have passed since that date (or how far it is in the future). If close to the current date, also displays the number of weeks and months.',
-        'Includes a brief summary of how the commonly used Gregorian calendar works and some basic facts about it.'
+        'The calendar grid for each selected year is generated automatically on the fly.',
+        'There is a menu to choose which weekday should be treated as the first day of the week (and works for any possible weekday).',
+        'For any day in the calendar grid it shows how many years and how many days have passed since that date or how far it is in the future. If close to the current date, also displays the number of weeks and months.',
+        'Includes a brief summary of how the commonly used Gregorian calendar works and some basic facts about it.',
+        'While this project looks very simple from the user\'s perspective, it has a lot of underlying complexity.'
       ]
     }
   },
