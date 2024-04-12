739a2eb48050ffe5961b9d62efd932a17b3fed28
offline2099 <offline2099@protonmail.com>
2024-04-12 18:17:11 +0700

    Minor refactoring

--- src/app/image-carousel/image-carousel.component.ts
+++ src/app/image-carousel/image-carousel.component.ts
@@ -12,7 +12,7 @@ export class ImageCarouselComponent {
   @HostBinding('class.inside-overlay') @Input() isOverlay: boolean = false;
 
   @Input() images: Project['images'] = [];
-  @Input() selectedImage: number = 1;
+  @Input() selectedImage: number = 0;
 
   @Output() imageChanged = new EventEmitter<number>();
 
@@ -20,18 +20,18 @@ export class ImageCarouselComponent {
 
   nextImage(): void {
     let newNumber: number = this.selectedImage + 1;
-    if (newNumber > this.images.length) newNumber = 1;
+    if (newNumber >= this.images.length) newNumber = 0;
     this.selectImage(newNumber);
   }
 
   previousImage(): void {
     let newNumber: number = this.selectedImage - 1;
-    if (newNumber <= 0) newNumber = this.images.length;
+    if (newNumber < 0) newNumber = this.images.length - 1;
     this.selectImage(newNumber);
   }
 
   selectImage(n: number): void {
-    if (n > 0 && n <= this.images.length && n != this.selectedImage) {
+    if (n >= 0 && n < this.images.length && n != this.selectedImage) {
       this.selectedImage = n;
       this.imageChanged.emit(n);
     }
