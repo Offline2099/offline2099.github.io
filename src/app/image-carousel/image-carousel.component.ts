f371926a4874dc26956da801ef1616dd60b54717
offline2099 <offline2099@protonmail.com>
2024-04-14 19:11:25 +0700

    Disable scrolling when overlay is active

--- src/app/image-carousel/image-carousel.component.ts
+++ src/app/image-carousel/image-carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, HostBinding, Output, EventEmitter } from '@angular/core';
+import { Component, Input, HostBinding, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks, BodyScrollOptions } from 'body-scroll-lock';
 
 import { Project } from '../interfaces';
 
@@ -10,6 +11,7 @@ import { Project } from '../interfaces';
 export class ImageCarouselComponent {
 
   @HostBinding('class.inside-overlay') @Input() isOverlay: boolean = false;
+  @ViewChild('scrollTarget') scrollTarget!: ElementRef;
 
   @Input() images: Project['images'] = [];
   @Input() selectedImage: number = 0;
@@ -17,6 +19,7 @@ export class ImageCarouselComponent {
   @Output() imageChanged = new EventEmitter<number>();
 
   overlayActive: boolean = false;
+  options: BodyScrollOptions = {reserveScrollBarGap: true}
 
   nextImage(): void {
     let newNumber: number = this.selectedImage + 1;
@@ -39,6 +42,15 @@ export class ImageCarouselComponent {
 
   toggleOverlay(): void {
     this.overlayActive = !this.overlayActive;
+    if (!this.isOverlay) {
+      if (this.overlayActive) 
+        disableBodyScroll(this.scrollTarget.nativeElement, this.options);
+      else enableBodyScroll(this.scrollTarget.nativeElement);
+    }    
+  }
+
+  ngOnDestroy() {
+    clearAllBodyScrollLocks();
   }
 
 }
