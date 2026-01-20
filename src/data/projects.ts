6f0628a6cc43922382a8cbad706932d5e1feeca3
offline2099 <offline2099@protonmail.com>
2026-01-20 15:42:21 +0700

    add snake project, update intro

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -123,5 +123,20 @@ export const PROJECTS: ProjectData[] = [
         'Currently, the demo is only designed for desktop devices. However, it can be expanded to include mobile and touchscreen devices.'    
       ]
     }
+  },
+  {
+    id: 'snake-game',
+    name: 'Snake',
+    numberOfImages: 8,
+    description: {
+      summary: 'A mini-game based on the classic Snake game. The player controls a snake that moves around and must collect food while avoiding obstacles or enemies.',
+      features: [
+        'The game has 20 levels, with gradually increasing difficulty. A new level is unlocked after the previous one is finished. Once completed, each level can be replayed. Best scores and completion times are saved.',
+        'New types of food, enemies, and obstacles are introduced as the game progresses. More complex mechanics, such as portals, appear later in the game. Some levels are more like logical puzzles that require a certain strategy.',
+        'Levels are created with an editor that allows to place available assets onto the game space and set various parameters before saving the data as JSON. Existing levels can also be loaded and edited that way.',
+        'Backgrounds are procedurally generated using an approach similar to drawing Voronoi diagrams.',
+        'While Angular is not the best environment for creating video games, it was an interesting and fun experience to attempt a project like this.'
+      ]
+    }
   }
 ];
